7f7a46313f2d6d3f4ba0128ba368aa8ced040485
brij26 <brijraptel007@gmail.com>
2025-11-07 16:47:44 +0530

    CICD run 11

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -16,7 +16,7 @@ from src.logger import logging
 # Set up Dagshub credentials for mlflow tracking
 dagshub_token = os.getenv("CAPSTONE_PROJ")
 if not dagshub_token:
-    raise EnvironmentError("CAPTSTONE_PROJ evironment variable is not set")
+    raise EnvironmentError("CAPSTONE_PROJ evironment variable is not set")
 
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
